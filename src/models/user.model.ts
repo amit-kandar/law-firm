cf6f9be3f469af4c68a7c834fc3e8e509b46deeb
Amit Kandar <kandaramit2001@gmail.com>
2024-02-27 22:04:36 +0530

    update email on user database

--- src/models/user.model.ts
+++ src/models/user.model.ts
@@ -16,7 +16,6 @@ const UserSchema = new Schema<UserDocument, Model<UserDocument>>({
     email: {
         type: String,
         required: true,
-        unique: true,
         lowercase: true
     },
     subject: {
