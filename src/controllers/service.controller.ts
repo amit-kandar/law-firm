e3c90ea00df86622c97c405092748f758804303e
Amit Kandar <kandaramit2001@gmail.com>
2024-02-29 17:54:22 +0530

    Added nodemailer

--- src/controllers/service.controller.ts
+++ src/controllers/service.controller.ts
@@ -4,6 +4,8 @@ import { Service } from "../models/service.model";
 import { APIError } from "../utils/APIError";
 import { APIResponse } from "../utils/APIResponse";
 import { asyncHandler } from "../utils/asyncHandler";
+import { createTransporter } from "../config/nodemailer";
+import nodemailer from 'nodemailer';
 
 export const service = asyncHandler(async (req: Request, res: Response, next: NextFunction): Promise<void> => {
     try {
@@ -24,6 +26,26 @@ export const service = asyncHandler(async (req: Request, res: Response, next: Ne
         if (!data) {
             throw new APIError(400, "Failed To Store Service Data");
         }
+
+        const emailOptions: nodemailer.SendMailOptions = {
+            from: `${process.env.MAIL_USER}`,
+            to: `${email}`,
+            subject: 'Thank You for Contacting Us',
+            html: `
+                <p>Dear ${first_name},</p>
+                <p>Thank you for reaching out to us. We appreciate your interest and will get back to you as soon as possible.</p>
+                <p>Best regards,<br>Kyrie Petrakis</p>
+            `,
+        };
+
+        const transporter = createTransporter();
+        transporter.sendMail(emailOptions, (error, info) => {
+            if (error) {
+                console.error('Error sending email:', error.message);
+            } else {
+                console.log('Email sent:', info.response);
+            }
+        });
 
         res.status(201).json(new APIResponse(201, { data }, "Successfully Submitted Service Data"));
     } catch (error) {
