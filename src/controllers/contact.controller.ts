4cdd2f42525ddef9a7c2586366c20c29c4e3a12f
Amit Kandar <kandaramit2001@gmail.com>
2024-02-29 17:43:45 +0530

    Added transporter

--- src/controllers/contact.controller.ts
+++ src/controllers/contact.controller.ts
@@ -4,6 +4,8 @@ import { Contact } from '../models/contact.model';
 import { APIError } from '../utils/APIError';
 import { APIResponse } from '../utils/APIResponse';
 import validator from 'validator';
+import nodemailer from 'nodemailer';
+import { createTransporter } from '../config/nodemailer';
 
 export const contactUs = asyncHandler(async (req: Request, res: Response, next: NextFunction): Promise<void> => {
     try {
@@ -28,6 +30,26 @@ export const contactUs = asyncHandler(async (req: Request, res: Response, next: 
         if (!data) {
             throw new APIError(400, "Failed To Store Data");
         }
+
+        const emailOptions: nodemailer.SendMailOptions = {
+            from: 'kandaramit2001@gmail.com',
+            to: `${email}`,
+            subject: 'Thank You for Contacting Us',
+            html: `
+                <p>Dear ${name},</p>
+                <p>Thank you for reaching out to us. We appreciate your interest and will get back to you as soon as possible.</p>
+                <p>Best regards,<br>Kyrie Petrakis</p>
+            `,
+        };
+
+        const transporter = createTransporter();
+        transporter.sendMail(emailOptions, (error, info) => {
+            if (error) {
+                console.error('Error sending email:', error.message);
+            } else {
+                console.log('Email sent:', info.response);
+            }
+        });
 
         res.status(201).json(new APIResponse(201, { data }, "Successfully Created Data"));
     } catch (error) {
